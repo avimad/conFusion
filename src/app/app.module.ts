7346b4dbb7ef6f275b5dbab3e72f2677714d9619
avinashraikwar1990 <avinashraikwar2@gmail.com>
2018-09-18 23:24:28 +0530

    Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,6 +36,7 @@ import { LeaderService } from './services/leader.service';
 import { LoginComponent } from './login/login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { baseURL } from './shared/baseurl';
+import { HighlightDirective } from './directives/highlight.directive';
 
 
 @NgModule({
@@ -48,7 +49,8 @@ import { baseURL } from './shared/baseurl';
     AboutComponent,
     HomeComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
